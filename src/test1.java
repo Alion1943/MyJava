fd8bbe8e89c3f5aae69a8a42bec0d3ce4c4564ab
Alion1943 <1072886373@qq.com>
2019-11-12 16:57:32 +0800

    存在与否

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
 public class test1 {
     public static void main(String[] args) {
-        
+
+        File file = new File("E:/123.txt");
+        System.out.println(file.exists()?"存在":"不存在");
+
+        if (!file.exists()) {
+
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(file.exists()?"存在":"不存在");
+        System.out.println(file.getAbsolutePath());
+
+
     }
 }
